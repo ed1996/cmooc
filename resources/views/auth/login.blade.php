281d05c4093a5d781f47542110305fb0783065c2
JrBour <eswolfm4jor@gmail.com>
2017-05-25 18:33:56 +0200

    Résolution des problémes de consoles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,50 +3,51 @@
 @section('content')
 
 
-<div class="container-fluid1">
+    <div class="container-fluid-register">
 
-    <div class="fond">
-        <div class="row backbox">
-            <div class=" col-lg-7 col-md-12 col-xs-12 textconnexion">
-                <p class="title">Vous n'avez pas de compte ?</p>
-                <p>Il est temps de s'inscrire pour connaitre et <br> apprendre de nouvelles technologies</p>
-                <a href="{{ url('/register') }}"><button>Inscrivez-vous</button></a></p>
+        <div class="fond">
+            <div class="row backbox">
+                <div class=" col-lg-7 col-md-12 col-xs-12 textconnexion">
+                    <p class="title">Vous n'avez pas de compte ?</p>
+                    <p>Il est temps de s'inscrire pour connaitre et <br> apprendre de nouvelles technologies</p>
+                    <a href="{{ url('/register') }}"><button>Inscrivez-vous</button></a></p>
+                </div>
+
+
+                <div class=" col-lg-5 col-md-12 col-xs-12 contact-form-connexion">
+                    <h2>CONNEXION</h2>
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+                        <ul>
+                            <li>
+                                <i class="fa fa-at form-icon  fa-lg"></i>
+                                <input id="email" type="email" placeholder="Email" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </li>
+                            <li>
+                                <i class="fa fa-lock form-icon fa-lg"></i>
+                                <input id="password" type="password"  placeholder="Mot de passe" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </li>
+
+                        </ul>
+                        <!--<h3>Mot de passe oublié ?</h3>-->
+                        <button type="submit">Connexion</button>
+                    </form>
+                </div>
             </div>
         </div>
 
-        <div class=" col-lg-5 col-md-12 col-xs-12 contact-form-connexion">
-            <h2>CONNEXION</h2>
-            <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                {{ csrf_field() }}
-                    <ul>
-                        <li>
-                            <i class="fa fa-at form-icon  fa-lg"></i>
-                            <input id="email" type="email" placeholder="Email" name="email" value="{{ old('email') }}" required autofocus>
-
-                            @if ($errors->has('email'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                            @endif
-                        </li>
-                        <li>
-                            <i class="fa fa-lock form-icon fa-lg"></i>
-                            <input id="password" type="password"  placeholder="Mot de passe" name="password" required>
-
-                            @if ($errors->has('password'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                            @endif
-                        </li>
-
-                    </ul>
-                    <!--<h3>Mot de passe oublié ?</h3>-->
-                    <button type="submit">Connexion</button>
-            </form>
-        </div>
     </div>
-
-</div>
 
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,55 +2,60 @@
 
 @section('content')
 
-<div class="container-fluid1">
-    <div class="fond">
-    <div class="row backbox">
+    <div class="container-fluid-register">
+        <div class="fond">
+            <div class="row backbox">
 
-        <div class=" col-lg-7 col-md-12 col-xs-12 textcontent">
-            <p class="title">Vous avez déja un compte ?</p>
-            <p> Connectez-vous et venez découvrir des <br>nouveaux cours, ou bien proposez en un nouveau.
-                <button><a href="{{ url('/login') }}">Connectez-vous</a></button></p>
-        </div>
+                <div class=" col-lg-7 col-sm-12 col-md-12 col-xs-12 textcontent">
+                    <p class="title">Vous avez déja un compte ?</p>
+                    <p> Connectez-vous et venez découvrir des <br>nouveaux cours, ou bien proposez en un nouveau.
+                        <a href="{{ url('/login') }}"><button>Connectez-vous</button></a></p>
+                </div>
 
-        <div class=" col-lg-5 col-md-12 col-xs-12 contact-form">
-            <h2>INSCRIPTION</h2>
+                <div class=" col-lg-5 col-sm-7 col-md-12 col-xs-12 contact-form">
+                    <h2>INSCRIPTION</h2>
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
-                               <ul>
-                                    <li>
-                                        <i class="fa fa-user form-icon fa-lg"></i>
-                                        <input id="name" type="text" placeholder="Nom" class="input-text js-input" name="name" value="{{ old('name') }}" required autofocus>
-                                        @if ($errors->has('name'))
-                                            <span class="help-block">
+                            <select name="isAdmin" style="display:none;">
+                                <option value="0">Utilisateur</option>
+                                <option value="1">Administrateur</option>
+                            </select>
+
+                        <ul>
+                            <li>
+                                <i class="fa fa-user form-icon fa-lg"></i>
+                                <input id="name" type="text" placeholder="Nom" class="input-text js-input" name="name" value="{{ old('name') }}" required autofocus>
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
                                                 <strong>{{ $errors->first('name') }}</strong>
                                             </span>
-                                        @endif
-                                    </li>
+                                @endif
+                            </li>
 
-                                    <li>
-                                        <i class="fa fa-at form-icon  fa-lg"></i>
-                                        <input id="email" type="text" placeholder="Email" class="input-text js-input" name="email" value="{{ old('email') }}" required>
-                                        @if ($errors->has('email'))
-                                            <span class="help-block">
+                            <li>
+                                <i class="fa fa-at form-icon  fa-lg"></i>
+                                <input id="email" type="email" placeholder="Email" class="input-text js-input" name="email" value="{{ old('email') }}" required>
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
                                                 <strong>{{ $errors->first('email') }}</strong>
                                             </span>
-                                        @endif
-                                    </li>
-                                    <li> <i class="fa fa-lock form-icon fa-lg"></i>
-                                        <input id="password" type="text" placeholder="Mot de passe" class="input-text js-input" name="password" required>
-                                        @if ($errors->has('password'))
-                                            <span class="help-block">
+                                @endif
+                            </li>
+                                <li> <i class="fa fa-lock form-icon fa-lg"></i>
+                                    <input id="password" type="password" placeholder="Mot de passe" class="input-text js-input" name="password" required>
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
                                                 <strong>{{ $errors->first('password') }}</strong>
                                             </span>
-                                        @endif
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-lock form-icon fa-lg"></i>
-                                        <input id="password-confirm" type="text" placeholder="Confirmer le mot de passe" class="input-text js-input" name="password_confirmation" required>
+                                    @endif
+                                </li>
+                                <li>
+                                    <i class="fa fa-lock form-icon fa-lg"></i>
+                                    <input id="password-confirm" type="password" placeholder="Confirmer le mot de passe" class="input-text js-input" name="password_confirmation" required>
 
-                                    </li>
-                                </ul>
+                                </li>
+                        </ul>
 
                         <button type="submit">Inscrivez-vous</button>
 
